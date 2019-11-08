1d61903e42aeed2f5ad0223cd665b0f1ca855b1a
Jackson Baker <jackson@jacksonbaker.net>
2019-11-08 12:57:33 -0800

    Partly completed ex39

---
+++ ex39/ex39.py
@@ -0,0 +1,28 @@
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+print('-' * 10)
+print('NY State has: ', cities['NY'])
+print('OR State has: ', cities['OR'])
+
+print('_' * 10)
+print("Michigan's abbreviation is: ", states['Michigan'])
+print("Florida's abbreviation is: ", states['Florida'])
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
