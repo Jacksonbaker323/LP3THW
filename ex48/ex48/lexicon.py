f0e4f2d50d3718f4aeb8e8a375c3843673917590
Jackson Baker <jackson@jacksonbaker.net>
2019-12-12 13:24:31 -0800

    Finished ex 48

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -4,15 +4,21 @@ def scan(sentence):
     results = []
     
     #split that string into an array
-    split_string = sentence.lower().split(" ")
+    split_string = sentence.split(" ")
     
     #look for any direction words in the array
     for word in split_string:
-        if word in ('north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back'):
+        if word.lower() in ('north', 'south', 'east', 'west', 'down', 'up', 'left', 'right', 'back'):
             results.append(('direction', word))
-        elif word in ('go', 'kill', 'eat', 'stop'):
+        elif word.lower() in ('go', 'kill', 'eat', 'stop'):
             results.append(('verb', word))
-        elif word in ('the', 'in', 'of', 'from', 'at', 'it'):
+        elif word.lower() in ('the', 'in', 'of', 'from', 'at', 'it'):
             results.append(('stop', word))
+        elif word.lower() in ('bear', 'princess'):
+            results.append(('noun', word))
+        elif word.isdigit():
+            results.append(('number', word))
+        else:
+            results.append(('error', word))
     #return all the direction words as an array of tuples with 'direction' and the direction
     return results
